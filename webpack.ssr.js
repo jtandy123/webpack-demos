76c745deaa5d2b1ec6de325075b7875e02e68dc2
jtandy123 <982071036@qq.com>
2020-11-02 21:34:15 +0800

    SSR

--- webpack.ssr.js
+++ webpack.ssr.js
@@ -89,20 +89,6 @@ const config = {
           },
         },
       ],
-      /*
-           // 将CSS代码整合进JS文件会造成JS文件的大小变大，操作DOM也会造成性能上的问题，
-           // 建议用extract-text-webpack-plugin插件将css文件打包为一个单独的文件
-            loader: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: [{
-                    // 这里可以使用postcss先处理下css代码
-                    loader: 'css-loader',
-                    options: {
-                        module: true
-                    }
-                }]
-            })
-            */
     }, {
       test: /\.less$/,
       use: [
@@ -155,41 +141,6 @@ const config = {
   },
   plugins: [
     new CleanWebpackPlugin(),
-    // webpack 4中已经移除webpack.optimize.CommonsChunkPlugin插件，推荐使用config.optimization.splitChunks来代替
-    /*
-        new webpack.optimize.CommonsChunkPlugin({
-            // manifest文件将每次打包都会更改的东西单独提取出来，保证没有更改的代码无需重新打包，可加快打包速度
-            names: ['vendor', 'manifest'],
-            // 配合manifest文件使用
-            minChunks: Infinity
-        }),
-        */
-    // 自动生成html文件，并自动引入js文件
-    /*
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-            inject: true,
-            minify: {
-                html5: true,
-                collapseWhitespace: true,
-                preserveLineBreaks: false,
-                minifyCSS: true,
-                minifyJS: true,
-                removeComments: false
-            }
-        }),
-        */
-    // 生成全局变量，mode属性已经提供了该功能
-    // new webpack.DefinePlugin({
-    //     'process.env.NODE_ENV': JSON.stringify("process.env.NODE_ENV")
-    // }),
-    // 输出的文件路径，该插件已Deprecated，推荐使用mini-css-extract-plugin
-    /*
-        new ExtractTextPlugin({
-            filename: 'css/[name].[hash:8].css',
-            allChunks: true
-        }),
-        */
     new MiniCssExtractPlugin({
       filename: '[name].[contenthash:8].css',
     }),
@@ -200,20 +151,6 @@ const config = {
       },
     }),
     new webpack.optimize.ModuleConcatenationPlugin(),
-    // new webpack.HotModuleReplacementPlugin(),
-    // new HtmlWebpackExternalsPlugin({
-    //     externals: [
-    //         {
-    //             module: 'react',
-    //             entry: 'https://11.url.cn/now/lib/16.2.0/react.min.js',
-    //             global: 'React'
-    //         }, {
-    //             module: 'react-dom',
-    //             entry: 'https://11.url.cn/now/lib/16.2.0/react-dom.min.js',
-    //             global: 'ReactDOM'
-    //         }
-    //     ]
-    // })
   ].concat(htmlWebpackPlugins),
   optimization: {
     splitChunks: false, // 必须设为false，SSR才会正常工作
